e22307cd789b6f212fe45e1a7e918455f28489c5
damianstone <damianstonedev@gmail.com>
2021-06-20 19:02:00 -0400

    deployed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import ReactGa from 'react-ga';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
 import Home from './pages/Home';
 import './App.css';
 
 // make the app much faster
 const NotFound = React.lazy(() => import('./pages/404/NotFound'));
 
-const App = () => {
-  useEffect(() => {
-    ReactGa.initialize('G-ZY56W7NQWC');
+const history = createBrowserHistory();
 
-    //to report page view
-    ReactGa.pageview('/');
-  }, []);
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
+const App = () => {
+  // Initialize google analytics page view tracking
 
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
         <Route path='/' component={Home} exact />
         <Route component={NotFound} />
